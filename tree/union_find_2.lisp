ae868885cfcb210baf5757dfeb2661a1094707c2
koubee2741 <51407064+kinase2741@users.noreply.github.com>
2020-09-27 01:05:37 +0900

    modified

--- tree/union_find_2.lisp
+++ tree/union_find_2.lisp
@@ -1,13 +1,52 @@
-(defstruct uf-tree
-  (size 30)
-  (parents (make-array size)))
+(defclass uf-tree ()
+  ((parents
+    :initarg parents
+    :accessor parents)
+   (group-count
+    :initarg group-count
+    :accessor group-count)))
 
 
-(defmethod uf-find ((tree uf-tree) (x integer))
-  (if (minusp (aref (uf-tree-parents tree) x))
+(defun uf-create (size)
+  (declare (fixnum size))
+  (make-instance 'uf-tree
+                 :parents (make-array size :initial-element -1)
+                 :group-count size))
+
+(defmethod uf-find ((uf uf-tree) (x fixnum))
+  (if (minusp (aref (parents uf) x))
       x
-      (setf (aref (uf-tree-parents tree) x)
-            (uf-find tree (aref (uf-tree-parents tree) x)))))
+      (setf (aref (parents uf) x)
+            (uf-find (aref (parents uf) x)))))
+
+(defmethod uf-show-parents ((uf uf-tree))
+  (map 'vector
+       (lambda (x)
+         (if (minusp x)
+             x
+             (uf-find uf x)))
+       (parents uf)))
+  
+
+(defmethod uf-unite ((uf uf-tree) (x fixnum) (y fixnum))
+  (when (> x y)
+    (rotatef x y))
+  (let ((x-parent (uf-find uf x))
+        (y-parent (uf-find uf y)))
+    (unless (= x-parent y-parent)
+      (incf (aref (parents uf) x-parent)
+            (aref (parents uf) y-parent))
+      (setf (aref (parents uf) y-parent) x-parent)
+      (decf (group-count uf)))))
+
+(defmethod uf-get-tree-size ((uf uf-tree) (x fixnum))
+  (- (aref (parents uf) (uf-find uf x))))
 
 
-(defme)
+(defmethod uf-count-trees ((uf uf-tree))
+  (group-count uf))
+
+
+(defmethod uf-friends-p ((uf uf-tree) (x fixnum) (y fixnum))
+  (= (uf-find uf x)
+     (uf-find uf y)))

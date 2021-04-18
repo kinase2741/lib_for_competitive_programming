e11f5db6167d1649c4e9ed39a8fab3ea70667734
motoshira <51407064+motoshira@users.noreply.github.com>
2021-04-18 09:57:01 +0900

    Add non-destructive functions

--- binary-trie.lisp
+++ binary-trie.lisp
@@ -12,15 +12,17 @@
 (defpackage :binary-trie
   (:nicknames :bt)
   (:use :cl)
-  (:shadow :remove)
-  (:export :make-bt :get-size :empty-p :insert :insert! :remove :remove! :get-element :get-min :get-max :lower-bound :upper-bound :count-value))
+  (:export #:add
+           #:add!
+           #:sub
+           #:sub!
+           #:ref
+           #:dump))
 
 (in-package :binary-trie)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defparameter *base-minus* 31))
-
-
 
 (defstruct (node (:conc-name n-)
                  (:constructor make-n (cnt &key l r)))
@@ -28,7 +30,52 @@
   (l nil)
   (r nil))
 
-(defun %add! (node val b)
+;; For export
+
+(defun add (binary-trie value)
+  "binary-trieにvalueを追加したものを返す。"
+  (%add binary-trie value #.*base-minus*))
+
+(defmacro add! (binary-trie value)
+  "破壊的にbinary-trieにvalueを追加する。REPL上ではListにdumpしたものを返す。"
+  (multiple-value-bind (args argvs val setter getter)
+      (get-setf-expansion binary-trie)
+    `(let ,(mapcar #'list args argvs)
+       (let ((,@val (add ,getter ,value)))
+         ,setter
+         #+swank (dump ,binary-trie)))))
+
+(defun sub (binary-trie value)
+  "binary-trieからvalueを削除したものを返す。"
+  (%sub binary-trie value #.*base-minus*))
+
+(defmacro sub! (binary-trie value)
+  "破壊的にbinary-trieからvalueを取り除く。REPL上ではListにdumpしたものを返す。"
+  (multiple-value-bind (args argvs val setter getter)
+      (get-setf-expansion binary-trie)
+    `(let ,(mapcar #'list args argvs)
+       (let ((,@val (sub ,getter ,value)))
+         ,setter
+         #+swank (dump ,binary-trie)))))
+
+(defun ref (binary-trie index)
+  "Binary Trieに格納された値について、順に数えてindex番目のものを返す。"
+  (%ref binary-trie index #.*base-minus*))
+
+(defun get-size (binary-trie)
+  "Binary Trieのサイズを返す。NILの場合は0を返す。"
+  (if (null binary-trie)
+      0
+      (n-cnt binary-trie)))
+
+(defun dump (binary-trie)
+  "Binary-trieの各要素をリストに格納して返す。デバッグ用。"
+  (loop for index from 0 below (get-size binary-trie)
+        collect (ref binary-trie index)))
+
+;; Helper functions
+
+(defun %add (node val b)
   (cond
     ((minusp b) (make-n 1))
     (:else
@@ -39,25 +86,16 @@
                        (n-cnt node)))
                :l (if bool
                       (when node (n-l node))
-                      (%add! (when node (n-l node))
-                             val
-                             (1- b)))
+                      (%add (when node (n-l node))
+                            val
+                            (1- b)))
                :r (if bool
-                      (%add! (when node (n-r node))
-                             val
-                             (1- b))
+                      (%add (when node (n-r node))
+                            val
+                            (1- b))
                       (when node (n-r node))))))))
 
-(defmacro add! (binary-trie value)
-  "Binary Trieにvalueを追加する。"
-  (multiple-value-bind (args argvs val setter getter)
-      (get-setf-expansion binary-trie)
-    `(let ,(mapcar #'list args argvs)
-       (let ((,@val (%add! ,getter ,value #.*base-minus*)))
-         ,setter
-         #+swank (dump ,binary-trie)))))
-
-(defun %sub! (node val b)
+(defun %sub (node val b)
   (cond
     ((or (minusp b)
          (= 1 (n-cnt node)))
@@ -68,24 +106,14 @@
        (make-n (1- (n-cnt node))
                :l (if bool
                       (n-l node)
-                      (%sub! (n-l node)
-                             val
-                             (1- b)))
+                      (%sub (n-l node)
+                            val
+                            (1- b)))
                :r (if bool
-                      (%sub! (n-r node)
-                             val
-                             (1- b))
+                      (%sub (n-r node)
+                            val
+                            (1- b))
                       (n-r node)))))))
-
-
-(defmacro sub! (binary-trie value)
-  "Binary Trieからvalueを取り除く。"
-  (multiple-value-bind (args argvs val setter getter)
-      (get-setf-expansion binary-trie)
-    `(let ,(mapcar #'list args argvs)
-       (let ((,@val (%sub! ,getter ,value #.*base-minus*)))
-         ,setter
-         #+swank nil))))
 
 (defun %ref (node index b)
   (cond
@@ -103,20 +131,8 @@
                          (1- b))
                    (ash 1 b)))))))
 
-(defun ref (binary-trie index)
-  "Binary Trieに格納された値について、順に数えてindex番目のものを返す。"
-  (%ref binary-trie index #.*base-minus*))
 
 
-(defun get-size (binary-trie)
-  "Binary Trieのサイズを返す。NILの場合は0を返す。"
-  (if (null binary-trie)
-      0
-      (n-cnt binary-trie)))
 
-(defun dump (binary-trie)
-  "Binary-trieの各要素をリストに格納して返す。デバッグ用。"
-  (loop for index from 0 below (get-size binary-trie)
-        collect (ref binary-trie index)))
 
 (in-package :cl-user)

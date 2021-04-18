635acb1d999ece8911800485b99533073faef317
motoshira <51407064+motoshira@users.noreply.github.com>
2021-04-18 09:15:12 +0900

    Make binary trie

--- binary-trie.lisp
+++ binary-trie.lisp
@@ -17,161 +17,106 @@
 
 (in-package :binary-trie)
 
-(defconstant +base+ 32)
-(defconstant +base-minus+ (1- +base+))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defparameter *base-minus* 31))
 
-(declaim (ftype (function (&optional fixnum list list) list) make-bt)
-         (inline make-bt))
-(defun make-bt (&optional (count 0) left right)
-  (list count left right))
 
-(defun get-size (bt)
-  (bt-count bt))
 
-(declaim (ftype (function (list) boolean) empty-p)
-         (inline empty-p))
-(defun empty-p (bt)
-  (zerop (get-size bt)))
+(defstruct (node (:conc-name n-)
+                 (:constructor make-n (cnt &key l r)))
+  (cnt cnt)
+  (l nil)
+  (r nil))
 
-(declaim (inline bt-count bt-left bt-right))
-(defun bt-count (bt) (if (null bt) 0 (first bt)))
-(defun bt-left (bt) (if (null bt) nil (second bt)))
-(defun bt-right (bt) (if (null bt) nil (third bt)))
+(defun %add! (node val b)
+  (cond
+    ((minusp b) (make-n 1))
+    (:else
+     (let ((bool (logbitp b val)))
+       ;; cntが1増える
+       (make-n (1+ (if (null node)
+                       0
+                       (n-cnt node)))
+               :l (if bool
+                      (when node (n-l node))
+                      (%add! (when node (n-l node))
+                             val
+                             (1- b)))
+               :r (if bool
+                      (%add! (when node (n-r node))
+                             val
+                             (1- b))
+                      (when node (n-r node))))))))
 
-(declaim (ftype (function (list fixnum) list) insert))
-(defun insert (bt value)
-  (declare (list bt)
-           (fixnum value))
-  (labels ((%insert (bt value b)
-             (declare (list bt)
-                      (fixnum value b))
-             (let ((bt (if (null bt) (make-bt) bt)))
-               (declare (list bt))
-               (the list
-                    (if (minusp b)
-                        (make-bt (1+ (bt-count bt))
-                                 (bt-left bt)
-                                 (bt-right bt))
-                        (if (logbitp b value)
-                            (make-bt (1+ (bt-count bt))
-                                     (bt-left bt)
-                                     (%insert (bt-right bt)
-                                              value
-                                              (1- b)))
-                            (make-bt (1+ (bt-count bt))
-                                     (%insert (bt-left bt)
-                                              value
-                                              (1- b))
-                                     (bt-right bt))))))))
-    (declare (ftype (function (list fixnum fixnum) list) %insert))
-    (%insert bt value +base-minus+)))
+(defmacro add! (binary-trie value)
+  "Binary Trieにvalueを追加する。"
+  (multiple-value-bind (args argvs val setter getter)
+      (get-setf-expansion binary-trie)
+    `(let ,(mapcar #'list args argvs)
+       (let ((,@val (%add! ,getter ,value #.*base-minus*)))
+         ,setter
+         #+swank (dump ,binary-trie)))))
 
-(declaim (ftype (function (list fixnum) list) remove))
-(defun remove (bt value)
-  (declare (list bt)
-           (fixnum value))
-  (labels ((%remove (bt value b)
-             (declare (list bt)
-                      (fixnum value b))
-             (the list
-                  (if (minusp b)
-                      (make-bt (1- (bt-count bt))
-                               (bt-left bt)
-                               (bt-right bt))
-                      (if (logbitp b value)
-                          (make-bt (1- (bt-count bt))
-                                   (bt-left bt)
-                                   (%remove (bt-right bt)
-                                            value
-                                            (1- b)))
-                          (make-bt (1- (bt-count bt))
-                                   (%remove (bt-left bt)
-                                            value
-                                            (1- b))
-                                   (bt-right bt)))))))
-    (declare (ftype (function (list fixnum fixnum) list) %remove))
-    (%remove bt value +base-minus+)))
+(defun %sub! (node val b)
+  (cond
+    ((or (minusp b)
+         (= 1 (n-cnt node)))
+     nil)
+    (:else
+     (assert node)
+     (let ((bool (logbitp b val)))
+       (make-n (1- (n-cnt node))
+               :l (if bool
+                      (n-l node)
+                      (%sub! (n-l node)
+                             val
+                             (1- b)))
+               :r (if bool
+                      (%sub! (n-r node)
+                             val
+                             (1- b))
+                      (n-r node)))))))
 
-(define-modify-macro insert! (value) (lambda (bt value) (insert bt value)))
-(define-modify-macro remove! (value) (lambda (bt value) (remove bt value)))
 
-(declaim (ftype (function (list fixnum) fixnum) get-element))
-(defun get-element (bt k)
-  (declare (list bt)
-           (fixnum k))
-  (labels ((%get-value (bt k b acc)
-             (declare (list bt)
-                      (fixnum k b acc))
-             (the fixnum
-                  (if (minusp b)
-                      acc
-                      (let ((m (if (null (bt-left bt))
-                                   0
-                                   (bt-count (bt-left bt)))))
-                        (if (< k m)
-                            (%get-value (bt-left bt)
-                                        k
-                                        (1- b)
-                                        acc)
-                            (%get-value (bt-right bt)
-                                        (- k m)
-                                        (1- b)
-                                        (logior acc (ash 1 b)))))))))
-    (declare (ftype (function (list fixnum fixnum fixnum) fixnum) %get-value))
-    (%get-value bt k +base-minus+ 0)))
+(defmacro sub! (binary-trie value)
+  "Binary Trieからvalueを取り除く。"
+  (multiple-value-bind (args argvs val setter getter)
+      (get-setf-expansion binary-trie)
+    `(let ,(mapcar #'list args argvs)
+       (let ((,@val (%sub! ,getter ,value #.*base-minus*)))
+         ,setter
+         #+swank nil))))
 
-(declaim (inline get-min get-max))
-(defun get-min (bt)
-  (get-element bt 0))
+(defun %ref (node index b)
+  (cond
+    ((minusp b) 0)
+    (:else
+     (let ((m (if (n-l node)
+                  (n-cnt (n-l node))
+                  0)))
+       (if (< index m)
+           (%ref (n-l node)
+                 index
+                 (1- b))
+           (logior (%ref (n-r node)
+                         (- index m)
+                         (1- b))
+                   (ash 1 b)))))))
 
-(defun get-max (bt)
-  (get-element bt (1- (bt-count bt))))
+(defun ref (binary-trie index)
+  "Binary Trieに格納された値について、順に数えてindex番目のものを返す。"
+  (%ref binary-trie index #.*base-minus*))
 
-(declaim (ftype (function (list fixnum) fixnum) lower-bound))
-(defun lower-bound (bt value)
-  (declare (list bt)
-           (fixnum value))
-  (labels ((%count-lower (bt b acc)
-             (declare (list bt)
-                      (fixnum b acc))
-             (the fixnum
-                  (cond
-                    ((or (empty-p bt)
-                         (minusp b))
-                     acc)
-                    ((logbitp b value)
-                     (%count-lower (bt-right bt)
-                                   (1- b)
-                                   (+ acc (if (empty-p (bt-left bt))
-                                              0
-                                              (bt-count (bt-left bt))))))
-                    (t
-                     (%count-lower (bt-left bt)
-                                   (1- b)
-                                   acc))))))
-    (declare (ftype (function (list fixnum fixnum) fixnum) %count-lower))
-    (%count-lower bt +base-minus+ 0)))
 
-(declaim (inline upper-bound))
-(defun upper-bound (bt value)
-  (lower-bound bt (1+ value)))
+(defun get-size (binary-trie)
+  "Binary Trieのサイズを返す。NILの場合は0を返す。"
+  (if (null binary-trie)
+      0
+      (n-cnt binary-trie)))
 
-(defun count-value (bt value)
-  (labels ((%count (bt b)
-             (cond
-               ((empty-p bt) 0)
-               ((minusp b)
-                (bt-count bt))
-               ((logbitp b value)
-                (%count (bt-right bt)
-                        (1- b)))
-               (t
-                (%count (bt-left bt)
-                        (1- b))))))
-    (%count bt +base-minus+)))
+(defun dump (binary-trie)
+  "Binary-trieの各要素をリストに格納して返す。デバッグ用。"
+  (loop for index from 0 below (get-size binary-trie)
+        collect (ref binary-trie index)))
 
 (in-package :cl-user)
-
-;;;
-;;; EOF
-;;;

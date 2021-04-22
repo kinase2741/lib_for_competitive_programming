e68567495bf7544b05a9d069c20b30898c7781dd
motoshira <51407064+motoshira@users.noreply.github.com>
2021-04-22 23:08:49 +0900

    Edit dseg

--- dynamic-segment-tree.lisp
+++ dynamic-segment-tree.lisp
@@ -22,99 +22,107 @@ TODO:
                                 n0
                                 (rec (ash n0 1)))))
 
+(defmacro define-dynamic-segment-tree (name &key op op-identity max-value element-type result-type)
+  (flet ((symb (&rest args)
+           (intern
+            (with-output-to-string (s)
+              (mapc (lambda (arg)
+                      (princ (string-upcase arg) s))
+                    args)))))
+    (let* ((range-end (sb-int:named-let rec ((n0 1))
+                        (if (>= n0 max-value)
+                            n0
+                            (rec (ash n0 1)))))
+           (name-str (symbol-name name))
+           (%make-dseg (symb "%make-" name-str))
+           (conc-name (symb name-str "-"))
+           (%fold (symb "%" name-str "-fold"))
+           (fold (symb name-str "-fold"))
+           (%update (symb "%" name-str "-update"))
+           (update (symb name-str "-update"))
+           (update! (symb name-str "-update!")))
+      `(progn
+         (defstruct (,name (:conc-name ,conc-name)
+                           (:constructor ,%make-dseg (&key (val op-identity) l r)))
+           (val val :type ,element-type)
+           (l nil :type (or null ,name))
+           (r nil :type (or null ,name)))
 
-(defstruct (dseg-node (:conc-name dseg-)
-                      (:constructor %make-dseg (&key (val op-identity) l r)))
-  (val val)
-  (l nil)
-  (r nil))
+         (defun ,fold (dseg l r)
+           (,%fold dseg l r 0 *range-end*))
 
-(defun dump (dseg)
-  (let ((res nil))
-    (sb-int:named-let rec ((dseg dseg)
-                           (begin 0)
-                           (end *range-end*))
-      (when (dseg-l dseg)
-        (rec (dseg-l dseg)
-             begin
-             (ash (+ begin end) -1)))
-      (when (<= (abs (- begin end))
-                1)
-        (push (cons begin
-                    (dseg-val dseg))
-              res))
-      (when (dseg-r dseg)
-        (rec (dseg-r dseg)
-             (ash (+ begin end) -1)
-             end)))
-    (reverse res)))
+         (defun ,%fold (dseg l r begin end)
+           (cond
+             ((null dseg) ,op-identity)
+             ((and (<= l begin)
+                   (<= end r))
+              (dseg-val dseg))
+             ((or (<= r begin)
+                  (<= end l))
+              ,op-identity)
+             (:else
+              (let ((mid (ash (+ begin end) -1)))
+                (,op (if (null (dseg-l dseg))
+                         ,op-identity
+                         (,%fold (dseg-l dseg)
+                                 l
+                                 r
+                                 begin
+                                 mid))
+                     (if (null (dseg-r dseg))
+                         ,op-identity
+                         (,%fold (dseg-r dseg)
+                                 l
+                                 r
+                                 mid
+                                 end)))))))
 
-(defun fold (dseg l r)
-  (%fold dseg l r 0 *range-end*))
+         (defun ,update (dseg index value)
+           (,%update dseg
+                     index
+                     value
+                     0
+                     ,range-end))
 
-(defun %fold (dseg l r begin end)
-  (cond
-    ((null dseg) op-identity)
-    ((and (<= l begin)
-          (<= end r))
-     (dseg-val dseg))
-    ((or (<= r begin)
-         (<= end l))
-     op-identity)
-    (:else
-     (let ((mid (ash (+ begin end) -1)))
-       (funcall *fn*
-                (if (null (dseg-l dseg))
-                    op-identity
-                    (%fold (dseg-l dseg)
-                           l
-                           r
-                           begin
-                           mid))
-                (if (null (dseg-r dseg))
-                    op-identity
-                    (%fold (dseg-r dseg)
-                           l
-                           r
-                           mid
-                           end)))))))
+         (defmacro ,update! (dseg index value)
+           (multiple-value-bind (args argvs val setter getter)
+               (get-setf-expansion dseg)
+             (let ((update (intern (concatenate 'string
+                                                ,name-str
+                                                "-UPDATE"))))
+               `(let ,(mapcar #'list args argvs)
+                  (let ((,@val (,update ,getter ,index ,value)))
+                    ,setter)))))
 
-(defmacro update! (dseg index value)
-  (multiple-value-bind (args argvs val setter getter)
-      (get-setf-expansion dseg)
-    `(let ,(mapcar #'list args argvs)
-       (let ((,@val (update ,getter ,index ,value)))
-         ,setter
-         ;; (dump ,getter)
-         ))))
 
-(defun update (dseg index value)
-  (%update dseg
-           index
-           value
-           0
-           *range-end*))
 
-(defun %update (dseg index value begin end)
-  (when (> (abs (- begin end))
-           1)
-    (let ((mid (ash (+ begin end) -1)))
-      (if (< index mid)
-          (%make-dseg :val (if (null dseg)
-                               value
-                               (funcall *fn* value (dseg-val dseg)))
-                      :l (%update (when dseg (dseg-l dseg))
-                                  index
-                                  value
-                                  begin
-                                  mid)
-                      :r (when dseg (dseg-r dseg)))
-          (%make-dseg :val (if (null dseg)
-                               value
-                               (funcall *fn* value (dseg-val dseg)))
-                      :l (when dseg (dseg-l dseg))
-                      :r (%update (when dseg (dseg-r dseg))
-                                  index
-                                  value
-                                  mid
-                                  end))))))
+         (defun ,%update (dseg index value begin end)
+           (when (> (abs (- begin end))
+                    1)
+             (let ((mid (ash (+ begin end) -1)))
+               (if (< index mid)
+                   (,%make-dseg :val (if (null dseg)
+                                         value
+                                         (,op value (dseg-val dseg)))
+                                :l (,%update (when dseg (dseg-l dseg))
+                                             index
+                                             value
+                                             begin
+                                             mid)
+                                :r (when dseg (dseg-r dseg)))
+                   (,%make-dseg :val (if (null dseg)
+                                         value
+                                         (,op value (dseg-val dseg)))
+                                :l (when dseg (dseg-l dseg))
+                                :r (,%update (when dseg (dseg-r dseg))
+                                             index
+                                             value
+                                             mid
+                                             end))))))))))
+
+(define-dynamic-segment-tree dseg
+  :op (lambda (x y) (+ x y))
+  :op-identity 0
+  :max-value #.(1- (ash 1 32))
+  :element-type fixnum
+  :result-type fixnum)

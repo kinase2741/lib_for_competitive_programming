c6fc8e690358356381055721b739605ec00aea0c
motoshira <51407064+motoshira@users.noreply.github.com>
2021-06-25 23:34:11 +0900

    Add merge

--- treap.lisp
+++ treap.lisp
@@ -1,13 +1,15 @@
 (in-package #:cl-user)
 
 (defpackage #:treap
-  (:use #:cl))
+  (:use #:cl)
+  (:shadow #:merge))
 
 (in-package  #:treap)
 
-(defstruct (treap (:constructor make-treap (value &key (children (vector nil nil)) (cnt 1) (sum 0))))
+(defstruct (treap (:constructor make-treap (value &key (l nil) (r nil) (cnt 1) (sum 0))))
   (value value)
-  (children children :type t)
+  (l nil :type (or null treap))
+  (r nil :type (or null treap))
   (priority (random #.most-positive-fixnum))  ;; 勝手に決まる
   (cnt cnt)
   (sum sum))
@@ -15,6 +17,28 @@
 (defun treap->list (treap) (list 1))
 
 (defun list->treap (list) (make-treap 1))
+
+(defun merge (l r)
+  (let ((new-cnt (+ (treap-cnt l)
+                    (treap-cnt r)))
+        (new-sum (+ (treap-value l)
+                    (treap-value r))))
+    (if (> (treap-priority l)
+           (treap-priority r))
+        ;; lが上
+        (make-treap (treap-value l)
+                    :l (treap-l l)
+                    :r (merge (treap-r l)
+                              r)
+                    :cnt new-cnt
+                    :sum new-sum)
+        ;; rが上
+        (make-treap (treap-value r)
+                    :l (merge l
+                              (treap-l r))
+                    :r (treap-r r)
+                    :cnt new-cnt
+                    :sum new-sum))))
 
 (in-package #:cl-user)
 

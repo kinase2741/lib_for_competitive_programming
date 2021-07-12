4f0b0b63cd68a76443b40005c852c337b7c4e6ae
motoshira <51407064+motoshira@users.noreply.github.com>
2021-07-12 22:03:49 +0900

    Add template

--- sqrt-tree.lisp
+++ sqrt-tree.lisp
@@ -1,2 +1,22 @@
 (defpackage sqrt-tree
-  (:use :cl))
+  (:use :cl)
+  (:nicknames :st))
+
+(defstruct (sqrt-tree (:conc-name st-)
+                      (:constructor %make-st))
+  (main nil
+   :type (simple-array fixnum (*)))
+  (sub nil
+   :type (simple-array fixnum (*)))
+  ()
+  (op nil :type function))
+
+(defun build (size &Key op initial-element)
+  (unless op
+    (error "OP must be supplied.")))
+
+(defun fold (st l r)
+  nil)
+
+(defun update (st l r)
+  nil)

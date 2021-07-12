321a39cf7d8342920197d529039d8acd67a92568
motoshira <51407064+motoshira@users.noreply.github.com>
2021-07-12 22:09:00 +0900

    Add build-fn

--- sqrt-tree.lisp
+++ sqrt-tree.lisp
@@ -8,12 +8,17 @@
    :type (simple-array fixnum (*)))
   (sub nil
    :type (simple-array fixnum (*)))
-  ()
   (op nil :type function))
 
-(defun build (size &Key op initial-element)
+(defun build (size &Key (op #'+) (initial-element 0) (identity-element 0))
   (unless op
-    (error "OP must be supplied.")))
+    (error "OP must be supplied."))
+  (let* ((sub-size (1+ (isqrt size))))
+    (%make-st :main (make-array size :element-type 'fixnum
+                                               :initial-element initial-element)
+              :sub (make-array sub-size :element-type 'fixnum
+                                        :initial-element identity-element)
+                        :op op)))
 
 (defun fold (st l r)
   nil)
